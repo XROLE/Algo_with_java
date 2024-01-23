d687c454d3a49b759fda95854f53b10b8e4d3daa
xrole <xrolediamond@gmail.com>
2024-01-23 11:40:43 +0100

    sum of 2d diagonal elements

--- SumOf2DArray.java
+++ SumOf2DArray.java
@@ -1,20 +1,22 @@
 public class SumOf2DArray {
     public static void main(String[] args) {
         int[][] myArray2D= {{1,2,3},{4,5,6},{7,8,9}};
-        int arrSum = sum(myArray2D);
+        int arrSum = sumDiagonalElements(myArray2D);
 
         System.out.println(arrSum);
     }
 
-    public static int sum(int[][] arr){
-        int sum  = 0;
+        public static int sumDiagonalElements(int[][] array){
+            int sum = 0;
 
-        for(int i = 0; i < arr.length; i++){
-            for(int j = 0; j < arr[i].length; j++){
-                sum += arr[i][j];
+            for(int i = 0; i < array.length; i++){
+                sum += array[i][i];
+
+                System.out.println(array[i][i]);
+
             }
-        }
 
-        return sum;
-    }
+            return sum;
+        }
+   
 }
