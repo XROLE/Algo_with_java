44dc2cb48dab3268388ae5eb8685e9a2e6e666ed
xrole <xrolediamond@gmail.com>
2024-01-23 14:54:22 +0100

    solve remove duplicate item in an array

---
+++ DuplicateNumber.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class DuplicateNumber {
+    public static void main(String[] args) {
+        int[] myArray = {1, 1, 2, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(removeDuplicates(myArray)));
+    }
+
+    public static int[] removeDuplicates(int[] arr) {
+       int[] uniqueArray = new int[arr.length];
+       int index = 0;
+
+       for(int i = 0; i < arr.length; i++){
+        Boolean isDuplicate = false;
+
+            for(int j = i + 1; j < arr.length; j++ )
+            if(arr[i] == arr[j]){
+                isDuplicate = true;
+                break;
+            }
+
+            if(!isDuplicate){
+                uniqueArray[index] = arr[i];
+                index++;
+            }
+       }
+
+       return Arrays.copyOf(uniqueArray, index);
+    }
+}
--- MissingNumber/MissingNumbera.java
+++ MissingNumber/MissingNumbera.java
@@ -1,3 +1,4 @@
+package MissingNumber;
 public class MissingNumbera {
     public static void main(String[] args) {
         int[] myArray = {1,2,3,4,6};
