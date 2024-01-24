a21c54b2baf236502c5ec168b31ba1a34f078745
xrole <xrolediamond@gmail.com>
2024-01-24 17:03:42 +0100

    implement traversing linked list

--- Main.java
+++ Main.java
@@ -4,8 +4,13 @@ public class Main {
     public static void main(String[] args) {
         SinglyLinkedList sLL = new SinglyLinkedList();
         sLL.createSinglylinkedList(5);
-        System.out.println(sLL.head.value);
-        sLL.insertInLinkedList(6, 0);
+        // System.out.println(sLL.head.value);
+        sLL.insertInLinkedList(6, 1);
+        sLL.insertInLinkedList(7, 3);
+        sLL.insertInLinkedList(8, 4);
+        sLL.insertInLinkedList(9, 0);
+
+        sLL.traverseSinglyLinkedList();
 
         System.out.println(sLL.head.value);
     }
--- SinglyLinkedList/SinglyLinkedList.java
+++ SinglyLinkedList/SinglyLinkedList.java
@@ -1,10 +1,12 @@
 package SinglyLinkedList;
+
 public class SinglyLinkedList {
     public Node head;
     public Node tail;
-    
+
     public int size;
-    public Node createSinglylinkedList(int nodeValue){
+
+    public Node createSinglylinkedList(int nodeValue) {
         head = new Node();
 
         Node node = new Node();
@@ -19,18 +21,18 @@ public class SinglyLinkedList {
         return head;
     }
 
-    public void insertInLinkedList(int nodeValue, int location){
+    public void insertInLinkedList(int nodeValue, int location) {
         Node node = new Node();
         node.value = nodeValue;
 
-        if(head == null){
+        if (head == null) {
             createSinglylinkedList(nodeValue);
             return;
-        } else if(location == 0){
+        } else if (location == 0) {
             node.next = head;
             head = node;
 
-        } else if(location >= size){
+        } else if (location >= size) {
             node.next = null;
             tail.next = node;
             tail = node;
@@ -38,7 +40,7 @@ public class SinglyLinkedList {
             Node tempNode = head;
             int index = 0;
 
-            while(index < location - 1){
+            while (index < location - 1) {
                 tempNode = tempNode.next;
                 index++;
             }
@@ -48,5 +50,26 @@ public class SinglyLinkedList {
             node.next = nexNode;
         }
         size++;
+    }
+
+    public void traverseSinglyLinkedList() {
+        if(head == null){
+            System.out.println("SLL does not exist");
+        } else {
+            Node tempNode = head;
+
+            for(int i = 0; i < size; i++){
+                System.out.println(tempNode.value);
+
+                if(i != size - 1){
+                    System.out.println(" -> ");
+                }
+
+                tempNode = tempNode.next;
+            }
+        }
+
+        System.out.println("\n");
+
     }
 }
